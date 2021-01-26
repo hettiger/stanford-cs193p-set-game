f3268ed7dfe0d8f03eb3ca724a3562966706c95e
Martin Hettiger <martin@hettiger.com>
2021-01-26 17:47:36 +0100

    Initial Commit

---
+++ SETGame/SETGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SETGameApp.swift
+//  SETGame
+//
+//  Created by Martin Hettiger on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SETGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
